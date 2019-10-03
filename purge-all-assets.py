e36ffeb6404f17cad19bc25d6b40e024c7445773
jnm <john@tmoj.net>
2019-10-03 14:06:52 -0400

    Update purge script to support Python 3
    …and KoBo API v2

--- purge-all-assets.py
+++ purge-all-assets.py
@@ -1,21 +1,18 @@
+# Python 3 only ^_^
+
 import sys
 import getpass
 import requests
 
-URL = 'http://kf.kobotoolbox.org/assets/?format=json'
+URL = 'http://kf.kobotoolbox.org/api/v2/assets/?format=json'
 
-username = raw_input('Username for "{}": '.format(URL))
+username = input('Username for "{}": '.format(URL))
 password = getpass.getpass()
 auth = requests.auth.HTTPBasicAuth(username, password)
 
 response = requests.get(URL, auth=auth)
 for asset in response.json()['results']:
-    # raw_input() can't write unicode. Can you believe it?
-    # https://bugs.python.org/issue7768
-    sys.stdout.write(
-        u'Remove "{}" ({})? (y/n) '.format(asset['name'], asset['uid'])
-    )
-    yn = raw_input()
+    yn = input(f'Remove "{asset["name"]}" ({asset["uid"]})? (y/n) ')
     if yn == 'y':
         del_resp = requests.delete(asset['url'], auth=auth)
-        print '\t', del_resp.status_code
+        print(f'\t{del_resp.status_code}')
