c9900ea4beb4ef469cbc18bede1117a3571fa238
Tino Kreutzer <tino.kreutzer@gmail.com>
2019-11-22 14:37:48 -0500

    adding a method to pull from multiple projects
    and removing the need to pass asset ID and api token by command line

--- retrieve_kobo_audio.py
+++ retrieve_kobo_audio.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python
 
+import json
 import requests
 import os
 import sys
 import urllib.request
 from urllib.error import HTTPError
 
+headers = { 'Authorization': 'Token [your token]' }
 
-# checks if all the required arguments provided
-if len(sys.argv) != 3:
-    print('Please provide arguments. Usage: retrieve_kobo_audio.py <kpiAssetID> <apiToken>')
-    sys.exit(1)
+# read json list from here
+assetlist = '{"[kpi asset id]": "[project title]"}'
 
-# parse asset ID and api token from CL args.
-kpiAssetID = str(sys.argv[1])
-apiToken = 'Token ' + str(sys.argv[2])
+# read json list from file, in case of many projects. Comment previous line out.
+# with open('assets.json', 'r') as f:
+#    assetlist=f.read()
 
-url = 'https://kf.kobotoolbox.org/api/v2/assets/' + kpiAssetID + '/data.json'
-headers = { 'Authorization': apiToken }
-
-# directory name where the audio files be downloaded, using the kpiAssetID as a parent directory
-dirname = os.path.join('.', kpiAssetID, 'audio_files')
+# parse asset filecd 
+assets = json.loads(assetlist)
 
 # downloads audio file into a directory
 def download_file(url, req_header):
@@ -52,34 +49,52 @@ def download_file(url, req_header):
 
     return file_path
 
-try:
-    response = requests.get(url, headers=headers)
-    # If the response was successful, no Exception will be raised
-    response.raise_for_status()
-except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')
-except Exception as err:
-    print(f'Other error occurred: {err}')
-else:
-    # converts response as JSON object
-    response = response.json()
-    # iterates the response body to retrieve all the download urls and
-    # downloads the audio files.
-    for (key, value) in response.items():
-        if key == 'results':
-            for item in value:
-                for (key, value) in item.items():
-                    if key == '_attachments':
-                        for item in value:
-                            for (key, value) in item.items():
-                                if key == 'download_url' and (value.endswith('.mp3') or value.endswith('.wav') or value.endswith('.m4a') or value.endswith('.ogg') or value.endswith('.flac')):                                
-                                    audio_url = value
-                                    file = download_file(audio_url, headers)
-                                    if file == 'found':
-                                        pass
-                                    elif file is not None:
-                                        filename = file.split('/')[-1]
-                                        print(filename + " ===> Status: Download completed.")
-                                    else:
-                                        print("Status: Failed to download, try again.")
-                                        
+files_downloaded = 0
+files_skipped = 0
+
+for assetid in assets:
+	kpiAssetID = assetid
+	kpiTitle = assets[assetid]
+
+
+	# directory name where the audio files be downloaded, using the kpiAssetID as a parent directory
+	dirname = os.path.join('.', kpiTitle+'_'+kpiAssetID, 'audio_files')
+	url = 'https://kf.kobotoolbox.org/api/v2/assets/' + kpiAssetID + '/data.json'
+
+	try:
+	    response = requests.get(url, headers=headers)
+	    # If the response was successful, no Exception will be raised
+	    response.raise_for_status()
+	except HTTPError as http_err:
+	    print(f'HTTP error occurred: {http_err}')
+	except Exception as err:
+	    print(f'Other error occurred: {err}')
+	else:
+	    # converts response as JSON object
+	    response = response.json()
+	    # iterates the response body to retrieve all the download urls and
+	    # downloads the audio files.
+	    for (key, value) in response.items():
+	        if key == 'results':
+	            for item in value:
+	                for (key, value) in item.items():
+	                    if key == '_attachments':
+	                        for item in value:
+	                            for (key, value) in item.items():
+	                                if key == 'download_url' and (value.endswith('.mp3') or value.endswith('.wav') or value.endswith('.m4a') or value.endswith('.ogg') or value.endswith('.flac')):                                
+	                                    audio_url = value
+	                                    # temporary fix until URLs are fixed in kobocat
+	                                    # file = download_file(audio_url, headers)
+	                                    file = download_file(audio_url.replace('media_file=media_file=', 'media_file='), headers)
+	                                    if file == 'found':
+	                                        files_skipped += 1
+	                                        pass
+	                                    elif file is not None:
+	                                        filename = file.split('/')[-1]
+	                                        print(filename + " ===> Status: Download completed.")
+	                                        files_downloaded += 1
+	                                    else:
+	                                        print("Status: Failed to download, try again.")
+	                                        
+print("Files downloaded:", files_downloaded)
+print("Files skipped: ", files_skipped)
